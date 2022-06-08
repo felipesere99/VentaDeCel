fde1a5d33983f9b952c8a01b16ed4666660ae065
felipesere99 <87679132+felipesere99@users.noreply.github.com>
2022-06-08 20:46:39 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
 import {NavBar} from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer'
+import Articulos from './components/ItemListContainer'
 
 function App() {
   return <>
   <NavBar />
-  <ItemListContainer />
+  <Articulos />
   </>
   }
 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,14 +1,33 @@
 import React from "react";
 
-function ItemListContainer() {
-    return (
-    <div>
-         <h2>
-            Mas modelos llegaran proximamente!
-        </h2>
-    </div>
+class Modelo extends React.Component {
+    render () {
+        return (
+            <h1>{this.props.valor}</h1>
+        )
+    }
+}
+
+class Articulos extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            celulares: ['Iphone XS', 'Samsung S22', 'Huawei Y9']
+        }
+    }
+
+
+    render () {
+        const modelos = this.state.celulares.map( t => (
+            <Modelo valor={t} />
+        ))
+
+        return (
+        <div>
+            <ul>{modelos}</ul>
+        </div>
    
     )
 }
-
-export default ItemListContainer;
+}
+export default Articulos;
